5694d7cf9483908c067fb0fecd39646709ab224e
Shane Klein <sakp5f@mst.edu>
2016-03-10 20:04:37 -0600

    Upload project

---
+++ Sphero Squared/MainPage.xaml.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using RobotKit;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Sphero_Squared
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public SpheroController master;
+        public SpheroController follower;
+
+        private float _defaultX = 0;
+        private float _defaultY = 0;
+        private float _defaultZ = 0;
+
+        private float _currentX = 0;
+        private float _currentY = 0;
+        private float _currentZ = 0;
+
+        private float _offsetX = 0;
+        private float _offsetY = 0;
+        private float _offsetZ = 0;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void toggleSpheroMaster_Toggled(object sender, RoutedEventArgs e)
+        {
+            if (toggleSpheroMaster.IsOn)
+            {
+                master = new SpheroController(this, true);
+                master.connectSphero();
+                toggleSpheroMaster.Header = "Connecting";
+            }
+            else
+            {
+                master.disconnect();
+                master = null;
+                toggleSpheroMaster.Header = "Disconnected";
+            }
+
+        }
+
+        private void toggleSpheroFollower_Toggled(object sender, RoutedEventArgs e)
+        {
+            if (toggleSpheroFollower.IsOn)
+            {
+                follower = new SpheroController(this, false);
+                follower.connectSphero();
+                toggleSpheroFollower.Header = "Connecting";
+            }
+            else
+            {
+                follower.disconnect();
+                follower = null;
+                toggleSpheroFollower.Header = "Disconnected";
+            }
+
+        }
+
+
+        public bool isAlreadyConnected(string name)
+        {
+            return ((master != null && name == master.bluetoothName) || (follower != null && name == follower.bluetoothName));
+        }
+
+        public void spheroConnected(bool isMaster, string name)
+        {
+            string message = "Connected to " + name;
+            if (isMaster)
+            {
+                toggleSpheroMaster.Header = message;
+            }
+            else
+            {
+                toggleSpheroFollower.Header = message;
+            }
+        }
+
+        public void spheroConnectFailed(bool isMaster)
+        {
+            string message = "Failed to connect";
+            ToggleSwitch t;
+            if (isMaster)
+            {
+                t = toggleSpheroMaster;
+            }
+            else
+            {
+                t = toggleSpheroFollower;
+            }
+            t.Header = message;
+            t.IsOn = false;
+        }
+
+        private void masterOrientationSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            if(master != null)
+            master.direction = (int)masterOrientationSlider.Value;
+        }
+
+        private void followerOrientationSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            if(follower != null)
+            follower.direction = (int)followerOrientationSlider.Value;
+        }
+
+        private void toggleStabilizeMaster_Toggled(object sender, RoutedEventArgs e)
+        {
+            if(master != null)
+            master.setStabilization(toggleStabilizeMaster.IsOn);
+        }
+
+        public void updateGyroPosition(float x, float y, float z)
+        {
+            _currentX = x;
+            _currentY = y;
+            _currentZ = z;
+
+            textMasterGyroX.Text = _currentX.ToString("0000.0");
+            textMasterGyroY.Text = _currentY.ToString("0000.0");
+            textMasterGyroZ.Text = _currentZ.ToString("0000.0");
+
+            updateOffset();
+
+        }
+
+        public void updateOffset()
+        {
+            _offsetX += _currentX;
+            _offsetY += _currentY;
+            _offsetZ += _currentZ;
+
+            textMasterOffsetX.Text = _offsetX.ToString("0000.0");
+            textMasterOffsetY.Text = _offsetY.ToString("0000.0");
+            textMasterOffsetZ.Text = _offsetZ.ToString("0000.0");
+        }
+
+        public void setDefaultGyroPosition()
+        {
+            _defaultX = _currentX;
+            _defaultY = _currentY;
+            _defaultZ = _currentZ;
+
+            _offsetX = 0;
+            _offsetY = 0;
+            _offsetZ = 0;
+
+            textMasterGyroDefaultX.Text = _defaultX.ToString("0000.0");
+            textMasterGyroDefaultY.Text = _defaultY.ToString("0000.0");
+            textMasterGyroDefaultZ.Text = _defaultZ.ToString("0000.0");
+        }
+
+        private void buttonResetMasterPosition_Click(object sender, RoutedEventArgs e)
+        {
+            setDefaultGyroPosition();
+        }
+    }
+}
---
+++ Sphero Squared/SpheroController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RobotKit;
+using RobotKit.Internal;
+using System.Diagnostics;
+
+namespace Sphero_Squared
+{
+    public class SpheroController : EventArgs
+    {
+        private Sphero _sphero;
+        private MainPage _mainPage;
+
+        private bool _isMaster;
+
+        private GyrometerReading _gyrometerReading;
+
+
+        public Sphero sphero
+        {
+            get
+            {
+                return _sphero;
+            }
+        }
+
+        public string name
+        {
+            get
+            {
+                return _sphero.Name;
+            }
+        }
+
+        public string bluetoothName
+        {
+            get
+            {
+                if (_sphero != null)
+                {
+                    return _sphero.BluetoothName;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public int direction
+        {
+            set
+            {
+                if(_sphero != null)
+                sphero.Roll(value, 0);
+            }
+        }
+
+        public SpheroController(MainPage mainPage, bool isMaster)
+        {
+            _mainPage = mainPage;
+            _isMaster = isMaster;
+        }
+
+        public void connectSphero()
+        {
+            RobotProvider provider = RobotProvider.GetSharedProvider();
+            provider.ConnectedRobotEvent += _onSpheroConnected;
+            provider.DiscoveredRobotEvent += _onSpheroDiscovered;
+            provider.NoRobotsEvent += _noSpheroDiscovered;
+            provider.FindRobots();
+        }
+
+        private void _onSpheroDiscovered(object sender, Robot discoveredSphero)
+        {
+            if (_sphero == null)
+            {
+                Debug.WriteLine("Discovered " + (_isMaster ? "Master" : "Follower") + " Sphero: " + discoveredSphero.BluetoothName);
+                //if (!_mainPage.isAlreadyConnected(discoveredSphero.BluetoothName))
+                if((_isMaster && discoveredSphero.BluetoothName == "Sphero-WRB") || (!_isMaster && discoveredSphero.BluetoothName == "Sphero-YRP"))
+                {
+                    Debug.WriteLine("Going to connect to  " + (_isMaster ? "Master" : "Follower") + " Sphero: " + discoveredSphero.BluetoothName);
+                    RobotProvider provider = RobotProvider.GetSharedProvider();
+
+
+                    provider.DiscoveredRobotEvent -= _onSpheroDiscovered;
+                    provider.NoRobotsEvent -= _noSpheroDiscovered;
+
+                    provider.ConnectRobot(discoveredSphero);
+
+                    Debug.WriteLine("Connecting to  " + (_isMaster ? "Master" : "Follower") + " Sphero: " + discoveredSphero.BluetoothName);
+                }
+                else
+                {
+                    Debug.WriteLine("Cannot connect as " + (_isMaster ? "Master" : "Follower") + " Sphero. Already connected: " + discoveredSphero.BluetoothName);
+                }
+            }
+        } 
+
+        private void _noSpheroDiscovered(object sender, EventArgs e)
+        {
+            _mainPage.spheroConnectFailed(_isMaster);
+            _sphero = null;
+        }
+
+        private void _onSpheroConnected(object sender, Robot connectedSphero)
+        {
+            RobotProvider provider = RobotProvider.GetSharedProvider();
+            provider.ConnectedRobotEvent -= _onSpheroConnected;
+
+            _sphero = (Sphero)connectedSphero;
+            _mainPage.spheroConnected(_isMaster, name);
+
+            _sphero.SetBackLED(1);
+
+            if (_isMaster)
+            {
+              //  setStabilization(false);
+            }
+
+                _sphero.SensorControl.GyrometerUpdatedEvent += _sensorControl_GyrometerUpdated;
+
+
+            Debug.WriteLine("Connected to " + (_isMaster ? "Master" : "Follower") + " Sphero: " + connectedSphero.BluetoothName);
+        }
+
+        private void _sensorControl_GyrometerUpdated(object sender, GyrometerReading gyrometerReading)
+        {
+            _gyrometerReading = gyrometerReading;
+            string readingText = String.Format(bluetoothName + " Gyro Reading: ({0}, {1}, {2})", _gyrometerReading.X, _gyrometerReading.Y, _gyrometerReading.Z);
+            
+
+            if (_isMaster)
+            {
+                _mainPage.updateGyroPosition(_gyrometerReading.X, _gyrometerReading.Y, _gyrometerReading.Z);
+            }
+        }
+
+        public void disconnect()
+        {
+            if (_sphero != null)
+            {
+                setStabilization(true);
+                _sphero.SensorControl.StopAll();
+                _sphero.Sleep();
+                _sphero = null;
+            }
+        }
+
+        public void setStabilization(bool stabilize)
+        {
+            //Initialize paramByte. 0 means no stabilization (disable stabilization - lock motors)
+            byte paramByte = (byte)0;
+
+            //If stabilize is true, set paramByte to 1 (enable stabilization)
+            if(stabilize)
+            {
+                paramByte = (byte)1;
+            }
+
+            //Parameters have to be passed as an array of bytes.
+            Byte[] param = new Byte[] { paramByte };
+
+            //Write the message to send to the Sphero. The first 0x02 points to the motor. The second 0x02 points to stabilization.
+            //Values determined from the code of Sphero's SDK for other platforms.
+            DeviceMessage msg = new DeviceMessage(0x02, 0x02, param);
+
+            //Send the message to the Sphero.
+            _sphero.WriteToRobot(msg);
+        }
+        
+
+    }
+}
